af8efd5a913d80c5ea1fd30608da11da80875330
Eli Grey <me@eligrey.com>
2015-10-06 23:55:42 -0400

    Remove version tag in require URL
    rawgit drops everything after ? anyways when it comes to caching

--- myrandom.user.js
+++ myrandom.user.js
@@ -9,5 +9,5 @@
 // @match        https://*.reddit.com/*
 // @grant        none
 // @run-at       document-start
-// @require      https://cdn.rawgit.com/eligrey/myrandom.js/master/myrandom.js?v=1.0.20151006
+// @require      https://rawgit.com/eligrey/myrandom.js/master/myrandom.js
 // ==/UserScript==
